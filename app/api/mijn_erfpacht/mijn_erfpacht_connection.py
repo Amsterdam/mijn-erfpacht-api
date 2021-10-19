e59ffaeb592424db7d6e27f7ce9d6b34bf95975b
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2021-10-19 17:54:24 +0200

    Format code

--- app/api/mijn_erfpacht/mijn_erfpacht_connection.py
+++ app/api/mijn_erfpacht/mijn_erfpacht_connection.py
@@ -7,7 +7,7 @@ from api.mijn_erfpacht.config import credentials, API_URL
 
 
 class MijnErfpachtConnection:
-    """ This helper class represents the connection to the MijnErfpacht API """
+    """This helper class represents the connection to the MijnErfpacht API"""
 
     def _make_request(self, url: str, identifier: str):
         """
@@ -16,65 +16,63 @@ class MijnErfpachtConnection:
         :param identifier: BSN or KVK number
         :return:
         """
-        encrypted = encrypt(identifier)
-        encoded_encryption = base64.urlsafe_b64encode(
-            encrypted).decode('ASCII')
-
         # Get the api key from env
-        key = credentials['API_KEY']
+        key = credentials["API_KEY"]
         if not key:
             raise Exception(
-                'No api key found in environment variables or key ' +
-                'is None/empty string')
+                "No api key found in environment variables or key "
+                + "is None/empty string"
+            )
 
-        headers = {'X-API-KEY': key}
-        res = requests.get(
-            f'{url}/{encoded_encryption}',
-            headers=headers,
-            timeout=9
-        )
+        (encrypted, iv) = encrypt(identifier)
+        encoded_encryption = base64.urlsafe_b64encode(encrypted).decode("ASCII")
+
+        headers = {"X-API-KEY": key, "X-RANDOM-IV": iv}
+        res = requests.get(f"{url}/{encoded_encryption}", headers=headers, timeout=9)
         return res
 
     def check_erfpacht_bsn(self, bsn):
-        return self.check_erfpacht(bsn, 'user')
+        return self.check_erfpacht(bsn, "user")
 
     def check_erfpacht_kvk(self, kvk_nummer):
-        return self.check_erfpacht(kvk_nummer, 'company')
+        return self.check_erfpacht(kvk_nummer, "company")
 
-    def check_erfpacht(self, identifier, kind='user'):
-        """ Check for erfpacht at MijnErfpacht based on a BSN """
+    def check_erfpacht(self, identifier, kind="user"):
+        """Check for erfpacht at MijnErfpacht based on a BSN"""
         # Encrypt and decode the bsn
         # Check the MijnErfpacht API if the BSN has erfpacht
         # Handle forbidden response
-        assert kind in ['user', 'company']
-        url = f'{API_URL}/api/check/groundlease/{kind}'
+        assert kind in ["user", "company"]
+        url = f"{API_URL}/api/check/groundlease/{kind}"
         res = self._make_request(url, identifier)
 
         if res.status_code == 403:
             raise Exception(
-                'Unable to authenticate to source API. ' +
-                'Check if the provided api key is correct and if you are ' +
-                'making the request through a whitelisted ' +
-                'environment (e.g. secure VPN).')
+                "Unable to authenticate to source API. "
+                + "Check if the provided api key is correct and if you are "
+                + "making the request through a whitelisted "
+                + "environment (e.g. secure VPN)."
+            )
 
         # Handle 400 range responses
-        if str(res.status_code)[0] == '4':
+        if str(res.status_code)[0] == "4":
             raise Exception(
-                'The source API responded with 4xx status code, ' +
-                'saying: {}'.format(res.text))
+                "The source API responded with 4xx status code, "
+                + "saying: {}".format(res.text)
+            )
 
         return res.text == "true"
 
     def get_notifications_bsn(self, bsn):
-        return self.get_notifications(bsn, 'bsn')
+        return self.get_notifications(bsn, "bsn")
 
     def get_notifications_kvk(self, kvk_number):
-        return self.get_notifications(kvk_number, 'kvk')
+        return self.get_notifications(kvk_number, "kvk")
 
     def get_notifications(self, identifier, kind):
-        assert kind in ['bsn', 'kvk']
+        assert kind in ["bsn", "kvk"]
 
-        url = f'{API_URL}/api/notifications/{kind}'
+        url = f"{API_URL}/api/notifications/{kind}"
 
         res = self._make_request(url, identifier)
         if res.status_code == 204:
--- app/api/server.py
+++ app/api/server.py
@@ -23,11 +23,7 @@ api = Api(app)
 CORS(app=app)
 
 if SENTRY_DSN:
-    sentry_sdk.init(
-        dsn=SENTRY_DSN,
-        integrations=[FlaskIntegration()],
-        with_locals=True
-    )
+    sentry_sdk.init(dsn=SENTRY_DSN, integrations=[FlaskIntegration()], with_locals=True)
 
 logger = logging.getLogger(__name__)
 
@@ -42,21 +38,20 @@ into a swagger spec which is exposed on '/api/erfpacht/'.
 """
 # Configure Swagger
 swagger_config = {
-    "headers": [
-    ],
+    "headers": [],
     "specs": [
         {
-            "endpoint": 'apispec_1',
-            "route": '/api/erfpacht/apispec_1.json',
+            "endpoint": "apispec_1",
+            "route": "/api/erfpacht/apispec_1.json",
             "rule_filter": lambda rule: True,  # all in
             "model_filter": lambda tag: True,  # all in
-            "title": "MijnErfpacht API Client"
+            "title": "MijnErfpacht API Client",
         }
     ],
     "static_url_path": "/api/erfpacht/static",
     # "static_folder": "static",  # must be set by user
     "swagger_ui": True,
-    "specs_route": "/api/erfpacht"
+    "specs_route": "/api/erfpacht",
 }
 swagger = Swagger(app, config=swagger_config)
 
@@ -68,11 +63,11 @@ def get_data(kind, identifier):
     has_erfpacht = False
     notifications = []
 
-    if kind == 'bsn':
+    if kind == "bsn":
         has_erfpacht = con.check_erfpacht_bsn(identifier)
         if has_erfpacht:
             notifications = con.get_notifications_bsn(identifier)
-    elif kind == 'kvk':
+    elif kind == "kvk":
         has_erfpacht = con.check_erfpacht_kvk(identifier)
         if has_erfpacht:
             notifications = con.get_notifications_kvk(identifier)
@@ -81,7 +76,7 @@ def get_data(kind, identifier):
 
 
 class ErfpachtCheck(Resource):
-    """ Class representing the 'api/erfpacht/check-erfpacht' endpoint"""
+    """Class representing the 'api/erfpacht/check-erfpacht' endpoint"""
 
     def get(self):  # noqa: C901
         """
@@ -96,21 +91,21 @@ class ErfpachtCheck(Resource):
             description: Invalid SAML or BSN
         """
         parser = reqparse.RequestParser()
-        token_arg_name = 'x-saml-attribute-token1'
+        token_arg_name = "x-saml-attribute-token1"
         parser.add_argument(
             token_arg_name,
-            location='headers',
+            location="headers",
             required=True,
-            help='SAML token required'
+            help="SAML token required",
         )
         kind = None
         identifier = None
 
         try:
             identifier = get_kvk_number_from_request(request)
-            kind = 'kvk'
+            kind = "kvk"
         except SamlVerificationException:
-            return {'status': 'ERROR', 'message': 'Missing SAML token'}, 400
+            return {"status": "ERROR", "message": "Missing SAML token"}, 400
         except KeyError:
             # does not contain kvk number, might still contain BSN
             pass
@@ -119,19 +114,19 @@ class ErfpachtCheck(Resource):
             try:
                 identifier = get_bsn_from_request(request)
             except InvalidBSNException:
-                return {'status': 'ERROR', 'message': 'Invalid BSN'}, 400
-            kind = 'bsn'
+                return {"status": "ERROR", "message": "Invalid BSN"}, 400
+            kind = "bsn"
 
         try:
             has_erfpacht, notifications = get_data(kind, identifier)
         except Timeout:
-            return {'status': 'ERROR', 'message': 'Timeout'}, 500
+            return {"status": "ERROR", "message": "Timeout"}, 500
         except ConnectionError as e:
             logger.error(f"ConnectionError {e}")
-            return {'status': 'ERROR', 'message': 'Connection Error'}, 500
+            return {"status": "ERROR", "message": "Connection Error"}, 500
         except Exception as e:
             logger.exception(e)
-            return {'status': 'ERROR', 'message': 'Unknown error'}, 500
+            return {"status": "ERROR", "message": "Unknown error"}, 500
 
         return {
             "status": "OK",
@@ -143,15 +138,15 @@ class ErfpachtCheck(Resource):
 
 
 class Health(Resource):
-    """ Used in deployment to check if the API lives """
+    """Used in deployment to check if the API lives"""
 
     def get(self):
-        return 'OK'
+        return "OK"
 
 
 # Add resources to the api
-api.add_resource(ErfpachtCheck, '/api/erfpacht/check-erfpacht')
-api.add_resource(Health, '/status/health')
+api.add_resource(ErfpachtCheck, "/api/erfpacht/check-erfpacht")
+api.add_resource(Health, "/status/health")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
