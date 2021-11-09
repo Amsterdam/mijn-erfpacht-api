fb5da63b7d7916a097a3c8cbd23b169459fa4ad4
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2021-11-09 17:41:19 +0100

    Rm v1 service

--- scripts/get_erfpacht.py
+++ scripts/get_erfpacht.py
@@ -7,18 +7,13 @@ from app.mijn_erfpacht_connection import MijnErfpachtConnection
 logger.setLevel(logging.DEBUG)
 
 identifier = argv[1]
-kind = argv[2] if len(argv) >= 3 else None
-
 
 con = MijnErfpachtConnection()
 
 has_erfpacht = False
 notifications = []
 
-if kind == "bsn":
-    has_erfpacht = con.check_erfpacht_bsn(identifier)
-elif kind == "bsn_v1":
-    has_erfpacht = con.check_erfpacht_bsn_v1(identifier)
+has_erfpacht = con.check_erfpacht_bsn(identifier)
 
 
 print("has_erfpacht", has_erfpacht)
